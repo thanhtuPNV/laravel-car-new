ccf3f97914606421a4e67b3ef0c5342040ce122e
unknown <admin@PNVD161.pn.com.vn>
2022-07-09 11:12:40 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\ManufactureController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::apiResource('cars', ApiController::class);
+Route::apiResource('manufactures', ManufactureController::class);
